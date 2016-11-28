0db81a88e00c31b229804f383888d0d1290abbcc
JeongIn <uugg2922@korea.ac.kr>
2016-11-28 20:16:07 +0900

    no cmd

--- safebox/Form1.cs
+++ safebox/Form1.cs
@@ -19,38 +19,7 @@ namespace safebox
         private System.Windows.Forms.ContextMenu contextMenu1;
         private System.Windows.Forms.MenuItem menuItem1;
         public Form1()
-        {
-            //this.contextMenu1 = new System.Windows.Forms.ContextMenu();
-            //this.menuItem1 = new System.Windows.Forms.MenuItem();
-            //// Initialize contextMenu1
-            //this.contextMenu1.MenuItems.AddRange(
-            //            new System.Windows.Forms.MenuItem[] { this.menuItem1 });
-
-            //// Initialize menuItem1
-            //this.menuItem1.Index = 0;
-            //this.menuItem1.Text = "E&xit";
-            //this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
-
-            //// Set up how the form should be displayed.
-            //this.ClientSize = new System.Drawing.Size(292, 266);
-            //this.Text = "Notify Icon Example";
-
-            //// Create the NotifyIcon.
-            //this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-
-            //// The Icon property sets the icon that will appear
-            //// in the systray for this application.
-            //notifyIcon1.Icon = new Icon("appicon.ico");
-
-            //// The ContextMenu property sets the menu that will
-            //// appear when the systray icon is right clicked.
-            //notifyIcon1.ContextMenu = this.contextMenu1;
-
-            //// The Text property sets the text that will be displayed,
-            //// in a tooltip, when the mouse hovers over the systray icon.
-            //notifyIcon1.Text = "Form1 (NotifyIcon example)";
-            //notifyIcon1.Visible = true;
-            
+        {   
             InitializeComponent();
         }
         private void menuItem1_Click(object Sender, EventArgs e)
@@ -67,6 +36,8 @@ namespace safebox
                 process.StartInfo.Arguments = arg;
                 process.StartInfo.UseShellExecute = false;
                 process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
                 process.StartInfo.Verb = "runas";
 
                 try
@@ -99,6 +70,8 @@ namespace safebox
                 process.StartInfo.Arguments = arg;
                 process.StartInfo.UseShellExecute = false;
                 process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
                 process.StartInfo.Verb = "runas";
 
                 process.Start();
